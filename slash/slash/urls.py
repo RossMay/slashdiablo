bb7b7509df296ac3b9409f7a88224df9123250fb
SlashDiablo CS Server <slashdiablo@slashdiablo.net>
2016-01-28 17:03:58 -0500

    Theme!

--- slash/slash/urls.py
+++ slash/slash/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import include,url
 from django.contrib import admin
 
+from . import views
+
 urlpatterns = [
 	url(r'^diablo2/', include('diablo2.urls')),
 	url(r'^admin/', admin.site.urls),
+	url(r'^', include('base.urls')),
 ]
